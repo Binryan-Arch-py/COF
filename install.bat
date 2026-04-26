297bf6b067e54ab3ddecb8a9642321111e10452b
Binryan-Arch-py <bryanarana2606@gmail.com>
2026-04-25 23:12:27 -0600

    fix: solucionar problema de instalacion

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
-o off
+@echo off
 echo Instalando Python...
-winget install --id Python.Python.3.12 --silent --show-progress
+winget install --id Python.Python.3.12 --silent
 
 :: Refrescar variables de entorno para esta sesión (truco para que reconozca python)
 set "PATH=%PATH%;%LOCALAPPDATA%\Programs\Python\Python312\;%LOCALAPPDATA%\Programs\Python\Python312\Scripts\"
@@ -17,6 +17,6 @@ python -m venv env && call .\env\Scripts\activate && echo Entorno virtual creado
 echo Instalando dependencias...
 pip install -r requirements.txt && echo Instalacion de dependencias completada.
 
-echo Finalizada instalacion, ya puedes usar el programa COF.
+echo Finalizada instalacion, ya puedes usar el programa COFB.
 pause
 
